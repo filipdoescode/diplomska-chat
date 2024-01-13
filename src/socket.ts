b703f99cae6f8968234840eb5d24f11963ed02f9
filipdoescode <vasilovskif@gmail.com>
2024-01-13 19:01:06 +0100

    bugifx:socket connection url

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,8 @@
 import { io, Socket } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'http://localhost:3000';
+const URL =
+  process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
 
+// @ts-expect-error undefined gets it from windows.location
 export const socket: Socket = io(URL, { autoConnect: false });
