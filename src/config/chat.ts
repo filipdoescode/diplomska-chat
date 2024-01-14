843e1936a92605ce654768ec5d07b073156b0529
filipdoescode <vasilovskif@gmail.com>
2024-01-14 20:56:58 +0100

    moved assets to public folder

--- src/config/chat.ts
+++ src/config/chat.ts
@@ -1,5 +1,5 @@
 export const roomsList = [
-  { id: '1', name: 'Соба 1', image: './src/assets/room-1.webp' },
-  { id: '2', name: 'Соба 2', image: './src/assets/room-2.webp' },
-  { id: '3', name: 'Соба 3', image: './src/assets/room-3.webp' },
+  { id: '1', name: 'Соба 1', image: './public/assets/room-1.webp' },
+  { id: '2', name: 'Соба 2', image: './public/assets/room-2.webp' },
+  { id: '3', name: 'Соба 3', image: './public/assets/room-3.webp' },
 ];
