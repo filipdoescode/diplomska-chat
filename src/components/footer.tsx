c8bfe8768c4b0a0bc745f286e7d196b8b39d8ce6
Filip Vasilovski <vasilovskif@gmail.com>
2026-03-03 20:06:49 +0100

    updated footer yeart

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className='flex justify-center bg-gray-100 shadow-sm'>
-      <p> © 2024 - Филип Василовски</p>
+      <p> © 2026 - Филип Василовски</p>
     </footer>
   );
 }
